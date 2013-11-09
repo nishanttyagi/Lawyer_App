94bab852bdb87bf17af575d3d4bc8f2ddf4e0b86
Nishant Tyagi <nishanttyagi1023@gmail.com>
2013-11-09 12:50:32 +0530

    Initial Commit

---
+++ LawyerApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LawyerApp
+//
+//  Created by Nishant Tyagi on 11/9/13.
+//  Copyright (c) 2013 Nishant Tyagi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ LawyerApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  LawyerApp
+//
+//  Created by Nishant Tyagi on 11/9/13.
+//  Copyright (c) 2013 Nishant Tyagi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
